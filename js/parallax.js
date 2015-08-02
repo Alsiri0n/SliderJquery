3ed85557b67f904f3e4bf543586f915ef790d048
Nikita <nikitaignatiev@gmail.com>
2015-08-02 23:02:11 +0300

    Раскрывающиеся превью

--- js/parallax.js
+++ js/parallax.js
@@ -3,11 +3,9 @@
 /*jslint plusplus: true */
 $(function () {
     "use strict";
-    $('#nav li').hover(function () {
-        $(this).children('ul').stop(false, true).fadeIn(300);
-        $(this).children('a').addClass('selected');
+    $('.wrapper').hover(function () {
+        $(this).children().stop().animate({marginTop: '100%', opacity: 0}, 600);
     }, function () {
-        $(this).children('ul').stop(false, true).fadeOut(300);
-        $(this).children('a').removeClass('selected');
+        $(this).children().stop().animate({marginTop: '0%', opacity: 1}, 600);
     });
 });
